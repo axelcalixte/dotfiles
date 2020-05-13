390fb963dd6e0e71ad8af464e621905772658094
Axel Calixte <axelcalixte@disroot.org>
2020-05-13 11:27:48 +0200

    updated st and dwm for fontconfig

--- st/.local/share/st/config.h
+++ st/.local/share/st/config.h
@@ -5,10 +5,10 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:style=regular:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:size=10";
 /* Spare fonts */
 static char *font2[] = {
-	"FontAwesome:style=regular:pixerlsize=12:antialias=true:autohint=true",
+	"FontAwesome:style=regular:pixelsize=12:antialias=true:autohint=true",
 /*     "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*     "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
