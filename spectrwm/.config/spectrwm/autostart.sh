9982beaf74112b4381e1e20920ac0c378cfa071d
Axel Calixte <axelcalixte@disroot.org>
2020-08-04 23:47:44 +0200

    added autostart.sh for spectrwm and configured the bar :)

---
+++ spectrwm/.config/spectrwm/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+nitrogen --restore &
+xautolock -time 10 -locker slock -corners ---- &
+dunst -conf ~/.config/dunst/dunstrc &
+picom -b --config ~/.config/picom/picom.conf &
+
+# Fix Gnome Apps Slow  Start due to failing services
+# Add this when you include flatpak in your system
+# autorun = ws[1]:dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY   
