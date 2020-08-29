a302d35c2247f1d5881f84d4a559b7ef2f8c9a0f
Axel Calixte <axel@axelSUSE.home>
2020-08-29 16:56:06 +0200

    cleaned up dotfiles by removing fish, bash and claws-mail

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,6 +0,0 @@
-function fish_user_key_bindings
-	#source /usr/share/fish/completions/fzf.fish
-	#source /usr/share/fish/functions/fish_vi_key_bindings.fish
-	#fzf_key_bindings
-	fish_vi_key_bindings
-end
