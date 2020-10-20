1d5edffa13c51c02ae0a63ff35b617b2175606ed
Axel Calixte <axelcalixte@disroot.org>
2020-10-20 19:22:04 +0200

    added a 5th option to suspend

--- bin/.local/bin/poweroff.sh
+++ bin/.local/bin/poweroff.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #dmenu=$(dmenu -fn "monospace:size=10" -nb "#212733" -nf "#cbccc6" -sb "#ffcc66" -sf "#212733")
-choice=$(echo "lock\nlogout\nreboot\npoweroff" | dmenu -fn "monospace:size=10" -nb "#212733" -nf "#cbccc6" -sb "#ffcc66" -sf "#212733")
+choice=$(echo "lock\nsuspend\nlogout\nreboot\npoweroff" | dmenu -fn "monospace:size=10" -nb "#212733" -nf "#cbccc6" -sb "#ffcc66" -sf "#212733")
 
 case "$choice" in
-    lock) systemctl suspend && slock ;;
+    lock) slock ;;
+    suspend) systemctl suspend && slock ;;
     logout) pkill xinit ;;
     reboot | poweroff) systemctl $choice;;
 esac
