eb35cdd61b029e797d9d5128a9bea6fcbe738c54
Axel Calixte <axelcalixte@disr.it>
2023-09-19 22:38:48 +0200

    trying zenbones colorscheme + formatting of beauty.lua and disable indent-blankline

--- nvim/.config/nvim/lua/features/beauty.lua
+++ nvim/.config/nvim/lua/features/beauty.lua
@@ -1,131 +1,144 @@
 return {
-   {
-      -- Highlight, edit, and navigate code
-      'nvim-treesitter/nvim-treesitter',
-      dependencies = {
-         'nvim-treesitter/nvim-treesitter-textobjects',
-      },
-      build = ':TSUpdate',
-      config = function()
-         require('nvim-treesitter.configs').setup {
-            -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'lua', 'vimdoc', 'python', 'html', 'css', 'javascript', 'typescript' },
+	{
+		-- Highlight, edit, and navigate code
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				-- Add languages to be installed here that you want installed for treesitter
+				ensure_installed = { "lua", "vimdoc", "python", "html", "css", "javascript", "typescript" },
 
-            -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-            auto_install = true,
+				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+				auto_install = true,
 
-            highlight = { enable = true },
-            indent = { enable = true },
-            incremental_selection = {
-               enable = true,
-               keymaps = {
-                  init_selection = '<c-space>',
-                  node_incremental = '<c-space>',
-                  scope_incremental = '<c-s>',
-                  node_decremental = '<M-space>',
-               },
-            },
-            textobjects = {
-               select = {
-                  enable = true,
-                  lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                  keymaps = {
-                     -- You can use the capture groups defined in textobjects.scm
-                     ['aa'] = '@parameter.outer',
-                     ['ia'] = '@parameter.inner',
-                     ['af'] = '@function.outer',
-                     ['if'] = '@function.inner',
-                     ['ac'] = '@class.outer',
-                     ['ic'] = '@class.inner',
-                  },
-               },
-               move = {
-                  enable = true,
-                  set_jumps = true, -- whether to set jumps in the jumplist
-                  goto_next_start = {
-                     [']m'] = '@function.outer',
-                     [']]'] = '@class.outer',
-                  },
-                  goto_next_end = {
-                     [']M'] = '@function.outer',
-                     [']['] = '@class.outer',
-                  },
-                  goto_previous_start = {
-                     ['[m'] = '@function.outer',
-                     ['[['] = '@class.outer',
-                  },
-                  goto_previous_end = {
-                     ['[M'] = '@function.outer',
-                     ['[]'] = '@class.outer',
-                  },
-               },
-               swap = {
-                  enable = true,
-                  swap_next = {
-                     ['<leader>a'] = '@parameter.inner',
-                  },
-                  swap_previous = {
-                     ['<leader>A'] = '@parameter.inner',
-                  },
-               },
-            },
-         }
-      end
-   },
-   {
-      "rebelot/kanagawa.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-         require("kanagawa").setup({
-            transparent = false,
-            background = {
-               dark = "dragon", -- "wave" or "dragon"
-               light = "lotus",
-            },
-            overrides = function(colors)
-               local theme = colors.theme
-               return {
-                  -- Better Floating Windows
-                  NormalFloat = { bg = "none" },
-                  FloatBorder = { bg = "none" },
-                  FloatTitle = { bg = "none" },
-                  NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                  LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                  MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						node_decremental = "<M-space>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		enabled = false,
+		config = function()
+			require("kanagawa").setup({
+				transparent = false,
+				background = {
+					dark = "dragon", -- "wave" or "dragon"
+					light = "lotus",
+				},
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						-- Better Floating Windows
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
-                  -- Better pop-up menu
-                  Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-                  PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-                  PmenuSbar = { bg = theme.ui.bg_m1 },
-                  PmenuThumb = { bg = theme.ui.bg_p2 },
-               }
-            end,
-         })
-         vim.cmd [[colorscheme kanagawa]]
-      end
-   },
-   {
-      -- Set lualine as statusline
-      'nvim-lualine/lualine.nvim',
-      -- See `:help lualine.txt`
-      opts = {
-         options = {
-            icons_enabled = false,
-            theme = 'auto',
-            component_separators = '|',
-            section_separators = '',
-         },
-      },
-   },
-   {
-      -- Add indentation guides even on blank lines
-      'lukas-reineke/indent-blankline.nvim',
-      -- Enable `lukas-reineke/indent-blankline.nvim`
-      -- See `:help indent_blankline.txt`
-      opts = {
-         char = '┊',
-         show_trailing_blankline_indent = false,
-      },
-   },
+						-- Better pop-up menu
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+						PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+					}
+				end,
+			})
+			vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
+	{
+		"mcchrish/zenbones.nvim",
+		dependencies = {
+			"rktjmp/lush.nvim",
+		},
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme zenbones]])
+		end,
+	},
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
+	{
+		-- Add indentation guides even on blank lines
+		"lukas-reineke/indent-blankline.nvim",
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help indent_blankline.txt`
+		enabled = false,
+		opts = {
+			char = "┊",
+			show_trailing_blankline_indent = false,
+		},
+	},
 }
