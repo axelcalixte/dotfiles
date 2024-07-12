22ef5a95eae7bfa2eba9971306b40702e5b51f56
Axel Calixte <axelcalixte@mailbox.org>
2024-07-12 11:59:24 +0200

    lazy lock and mini whitespace while writing

--- nvim/.config/nvim/lua/axel/features/helpers.lua
+++ nvim/.config/nvim/lua/axel/features/helpers.lua
@@ -1,4 +1,8 @@
 return {
    { "echasnovski/mini.surround", version = "*", opts = {} },
-   { "echasnovski/mini.comment",  version = "*", opts = {} },
+   { "echasnovski/mini.trailspace", version = "*", opts = {
+      even_while_inserting = true,
+      only_in_normal_buffers = true,
+   } },
+   { "numToStr/Comment.nvim", opts = {}, event = "InsertEnter" },
 }
