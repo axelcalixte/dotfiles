609bba0206567cb51c4185ca2402d53cb91896f2
Axel Calixte <axelcalixte@mailbox.org>
2025-05-11 17:39:04 +0200

    updated installation script

--- installation.bash
+++ installation.bash
@@ -2,16 +2,34 @@
 # Utility functions to help setup my SpiralLinux debian system
 
 function update_system {
-    sudo apt update && sudo apt upgrade
-}
-
-function cleanup {
-    sudo apt autopurge mousepad xterm gdebi synaptic hplip lightdm lightdm-gtk-greeter
+    sudo apt update -y && sudo apt upgrade -y
 }
 
 function install_utils {
     sudo apt install \
-        git bash-completion fzf ripgrep nnn trash-cli fd-find stow
+        ani-cli \
+        bash-completion \
+        borgbackup \
+        curl \
+        fd-find \
+        fish \
+        fzf \
+        git \
+        hx \
+        kitty \
+        libfuse2t64 \
+        libopengl0 \
+        libssl-dev \
+        ncdu \
+        nnn \
+        pkg-config \
+        ripgrep \
+        rsync \
+        rustup \
+        stow \
+        tig \
+        trash-cli \
+        vis
 
     mkdir -p ~/.cache/bash && touch ~/.cache/bash/history
 }
@@ -39,15 +57,13 @@ function configure_gcadapter {
 
 ## For dev tools and dependencies, or trying stuff out
 function install_distrobox {
-    sudo apt install podman
-    curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sudo sh
+    sudo apt install distrobox --no-install-recommends
+    distrobox create -n toolbx --image docker.io/library/debian:trixie
 
-    # distrobox create -n toolbx --image docker.io/library/debian:stable
-
-    # print "inside debian toolbox:\n \
-    # 'sudo apt install distrobox locales fzf zsh-syntax-highlighting zsh-autosuggestions git build-essential stow nnn ripgrep fd-find' \
-    # 'sudo dpkg-reconfigure locales' to keep fr_FR and en_US UTF-8 \
-    # 'export LANG=fr_FR.UTF-8' to have ls be good"
+    print "inside debian toolbox:\n \
+    'sudo apt install distrobox locales' \
+    'sudo dpkg-reconfigure locales' to keep fr_FR and en_US UTF-8 \
+    'export LANG=fr_FR.UTF-8' to have ls be good"
 }
 
 function install_fnm {
@@ -69,18 +85,25 @@ function install_starship {
     curl -sS https://starship.rs/install.sh | BIN_DIR="$HOME"/.local/bin sh
 }
 
+function install_uv {
+    mkdir -p $HOME/.local/bin
+    curl -LsSf https://astral.sh/uv/install.sh | env INSTALLER_NO_MODIFY_PATH=1 UV_INSTALL_DIR="$HOME/.local/bin" sh
+}
+
 function install_desktop {
     sudo apt install gnome-core \
         gnome-tweaks gnome-shell-extension-manager gnome-shell-extension-prefs \
         gnome-calendar \
-        evolution \
         seahorse \
+        flatpak \
         gnome-software-plugin-flatpak \
         gnome-shell-extension-caffeine \
         gnome-shell-extension-gpaste \
         switcheroo-control \
         mesa-vulkan-drivers \
-        keepassxc
+        keepassxc \
+        syncthing \
+        zim \
 }
 
 function install_flatpaks {
@@ -88,15 +111,14 @@ function install_flatpaks {
         com.obsproject.Studio \
         org.gnome.World.PikaBackup \
         org.signal.Signal \
-        org.zim_wiki.Zim \
         com.microsoft.Edge \
         org.mozilla.Thunderbird \
         org.gimp.GIMP \
         org.telegram.desktop \
-        org.gnome.clocks \
         com.github.tchx84.Flatseal \
         com.valvesoftware.Steam \
-        org.libreoffice.LibreOffice
+        org.libreoffice.LibreOffice \
+        com.discordapp.Discord
 }
 
 function gnome_settings {
@@ -119,57 +141,57 @@ function gnome_settings {
     dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name 'Terminal'
 }
 
-function setup_fonts {
-    fonts_dir="~/.local/share/fonts"
-    mkdir -p "$fonts_dir"
-
-    curl -LO https://github.com/rsms/inter/releases/download/v4.0/Inter-4.0.zip > "$fonts_dir"/Inter-4.0.zip
-    unzip "$fonts_dir"/Inter-4.0.zip
-    mkdir "$fonts_dir"/Inter && mv "$fonts_dir"/extras/ttf/Inter* "$fonts_dir"/Inter
-    rm -rf "$fonts_dir"/extras "$fonts_dir"/web "$fonts_dir"/help.txt "$fonts_dir"/Inter-4.0.zip "$fonts_dir"/Inter.ttc "$fonts_dir"/*.ttf "$fonts_dir"/LICENSE.txt
-
-    curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip > "$fonts_dir"/NerdFontsSymbolsOnly.zip
-    unzip "$fonts_dir"/NerdFontsSymbolsOnly.zip -d "$fonts_dir"/NerdFontsSymbolsOnly
-
-    fc-cache -fv
-    dconf write /org/gnome/desktop/interface/font-name 'Inter 11'
-    dconf write /org/gnome/desktop/interface/document-font-name 'Inter  11'
-    dconf write /org/gnome/desktop/wm/preferences/titlebar-font 'Inter Bold 11'
-    dconf write /org/gnome/desktop/interface/monospace-font-name 'DejaVu Sans Mono 11'
-}
+# function setup_fonts {
+#     fonts_dir="~/.local/share/fonts"
+#     mkdir -p "$fonts_dir"
+#
+#     curl -LO https://github.com/rsms/inter/releases/download/v4.0/Inter-4.0.zip > "$fonts_dir"/Inter-4.0.zip
+#     unzip "$fonts_dir"/Inter-4.0.zip
+#     mkdir "$fonts_dir"/Inter && mv "$fonts_dir"/extras/ttf/Inter* "$fonts_dir"/Inter
+#     rm -rf "$fonts_dir"/extras "$fonts_dir"/web "$fonts_dir"/help.txt "$fonts_dir"/Inter-4.0.zip "$fonts_dir"/Inter.ttc "$fonts_dir"/*.ttf "$fonts_dir"/LICENSE.txt
+#
+#     curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip > "$fonts_dir"/NerdFontsSymbolsOnly.zip
+#     unzip "$fonts_dir"/NerdFontsSymbolsOnly.zip -d "$fonts_dir"/NerdFontsSymbolsOnly
+#
+#     fc-cache -fv
+#     dconf write /org/gnome/desktop/interface/font-name 'Inter 11'
+#     dconf write /org/gnome/desktop/interface/document-font-name 'Inter  11'
+#     dconf write /org/gnome/desktop/wm/preferences/titlebar-font 'Inter Bold 11'
+#     dconf write /org/gnome/desktop/interface/monospace-font-name 'DejaVu Sans Mono 11'
+# }
 
 
 possible_steps="update_system
-cleanup
 install_desktop
 install_flatpaks
 install_utils
 install_fnm
 install_kitty
 install_starship
+install_uv
 install_neovim
 get_dotfiles
 install_distrobox
 configure_gcadapter
 gnome_settings
-setup_fonts
 "
+# setup_fonts
 
 case "$1" in
     "update_system") update_system ;;
-    "cleanup") cleanup ;;
     "install_desktop") install_desktop ;;
     "install_flatpaks") install_flatpaks ;;
     "install_utils") install_utils ;;
     "install_fnm") install_fnm ;;
     "install_kitty") install_kitty ;;
     "install_starship") install_starship ;;
+    "install_uv") install_uv ;;
     "install_neovim") install_neovim ;;
     "get_dotfiles") download_dotfiles ;;
     "install_distrobox") install_distrobox ;;
     "configure_gcadapter") configure_gcadapter ;;
     "gnome_settings") gnome_settings ;;
-    "setup_fonts") setup_fonts ;;
+    # "setup_fonts") setup_fonts ;;
 
     *) printf "possible arguments are:\n%s" "$possible_steps";;
 esac
